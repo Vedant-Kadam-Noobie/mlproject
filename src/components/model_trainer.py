c851cee56ec77c5c1c620bd3e022c545e7b69f3d
unknown <risingved.rv@gmail.com>
2024-07-27 08:07:46 +0530

    Hyperparameter Training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -50,15 +50,55 @@ class ModelTrainer:
                 "Decision Tree": DecisionTreeRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
-                "K-Neighbors Classifier": KNeighborsRegressor(),
-                "XGBClassifier": XGBRegressor(),
-                "CatBoosting Classifier": CatBoostRegressor(verbose=False),
-                "AdaBoost Classifier": AdaBoostRegressor(),
+                "XGBRegressor": XGBRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor(),
+            }
+            
+            params={
+                "Decision Tree": {
+                    # criterion: The function to measure the quality of a split
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # n_estimators: The number of trees in the forest. Higher numbers can improve performance but increase computation time
+                    'n_estimators': [8,16,32,64,128,256],
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'max_features':['sqrt','log2',None],
+                },
+                "Gradient Boosting":{
+                    # n_estimators: The number of boosting stages to be run
+                    'n_estimators': [8,16,32,64,128,256],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    # learning_rate: The step size at each iteration while moving toward a minimum of a loss function
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256],
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100],
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'n_estimators': [8,16,32,64,128,256],
+                    # 'loss':['linear','square','exponential'],
+                }
+                
             }
             
             # Evaluates each model using the training and testing data, returning a report with model names and their R² scores.
             model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                             models=models)
+                                             models=models,param=params)
             
             # To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
@@ -83,8 +123,6 @@ class ModelTrainer:
             r2_square = r2_score(y_test, predicted)
             return r2_square
             
-
-
 
             
         except Exception as e:
--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,7 @@ import pandas as pd
 # Usage: Commonly used to save machine learning models, preprocessors, or any Python objects to a file and load them later.
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -25,14 +26,21 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
 
 # Function evaluates various ML models on training & testing data, then returns thier performance
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+            
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
-            model.fit(X_train, y_train)  # Train model
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            # model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
 
